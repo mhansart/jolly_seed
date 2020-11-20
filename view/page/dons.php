9f945c079f8254145ca0c80392cf7976350ae044
Daphné Delvaux <ddelvaux71@gmail.com>
2020-11-20 16:07:18 +0100

    premier rajout essai de page dons

---
+++ view/page/dons.php
@@ -0,0 +1,40 @@
+<section class="pageDons">
+        <section class="centrePageDons">
+          <div class="d-flex row espace">
+            <a href="#">Voir sur la carte</a>
+            <div>
+              <p class="select">Trier par: <span>Date </span></p>
+              <i class="fas fa-chevron-down"></i>
+              <div class="options">
+                <a href="#">Date</a>
+                <a href="#">Catégorie</a>
+                <a href="#">Proximité</a>
+              </div>
+            </div>
+          </div>
+          <button class="btnDons"><a href="#">Poster une annonce</a></button>
+          <section class="box">
+            <div class="dons">
+              <div class="d-flex row">
+                <img class="pomme" src="static/pomme-mauve.png" />
+                <h3>Reinettes étoilées</h3>
+                <i class="far fa-heart"></i>
+              </div>
+              <img class="imageDon" src="static/reinette-etoilee.jpg" />
+              <div class="d-flex row">
+                <div>
+                  <p>Date : 26/10/2020</p>
+                  <p>Lieu : Ottignies</p>
+                </div>
+                <button>
+                  <a href="#">Contact</a>
+                </button>
+              </div>
+              <article>
+                Venez cueillir ces délicieuses pommes. Pour ceux qui ne la
+                connaisse pas, elles sont petites mais délicieuses à souhait!
+              </article>
+            </div>
+          </section>
+        </section>
+      </section>

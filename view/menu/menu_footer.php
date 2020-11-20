6ec52661ecdd57c4dcf1ebfe6aa030ef0a90f175
Hansart Marine <hansart.marine@gmail.com4>
2020-11-20 17:30:38 +0100

    footer design

--- view/menu/menu_footer.php
+++ view/menu/menu_footer.php
@@ -1,20 +1,22 @@
 <footer>
-    <div class="row-1-footer w-100">
-    <div class="social w-100">
-            <a href="#"><i class="fab fa-facebook"></i></a>
-            <a href="#"><i class="fab fa-twitter"></i></a>
-            <a href="#"><i class="fab fa-instagram"></i></a>
+    <div class="footer-container w-80">
+        <div class="row-1-footer w-100">
+            <div class="link-footer w-100 d-flex">
+                <a class="w-100" href="#apropos">À propos</a>
+                <div class="line w-100"></div>
+                <a class="w-100" href="#temoignages">Témoignages</a>
+                <div class="line w-100"></div>
+                <a class="w-100" href="#mentions">Mentions Légales</a>
+                <div class="line w-100"></div>
+                <a class="w-100" href="#contact">Contact</a>
+                <div class="line w-100"></div>
+            </div>
+            <div class="social w-100">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
         </div>
-        <div class="link-footer w-100 d-flex">
-            <a class="w-100" href="#apropos">À propos</a>
-            <div class="line w-100"></div>
-            <a class="w-100" href="#temoignages">Témoignages</a>
-            <div class="line w-100"></div>
-            <a class="w-100" href="#mentions">Mentions Légales</a>
-            <div class="line w-100"></div>
-            <a class="w-100" href="#contact">Contact</a>
-            <div class="line w-100"></div>
-        </div>
-    </div>
-      <p class="w-100">Un projet de D. Delvaux, L. Debruyne, M. Hansart</p>
+        <p class="w-100">Un projet de D. Delvaux, L. Debruyne, M. Hansart</p>
+      </div>
     </footer>

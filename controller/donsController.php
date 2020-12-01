de4a40059236adc26dfe949ff6ebf087fd221a3a
Hansart Marine <hansart.marine@gmail.com4>
2020-12-01 20:26:19 +0100

    (master): commit after git pull

--- controller/donsController.php
+++ controller/donsController.php
@@ -1,61 +1,55 @@
-<?php 
- // récupérer les annoncess et afficher
- $d = new Annonce();
- $tabDons = $d->read();
- // var_dump($tabAnnonces);
- // générer le tableau html
+<?php
+// récupérer les annoncess et afficher
+$d = new Annonce();
+$tabDons = $d->read();
+// var_dump($tabAnnonces);
+// générer le tableau html
 
 
 $dons = "";
 foreach ($tabDons as $value) {
   //définir la couleur du logo-pomme
-  if ($value["ads_type"]==="don"){
-    switch($value["ads_category"])
-    {
-    case "seed" :
-      $imageDon = "pomme-bleue.png";
-    break;
-    case "ground" :
-      $imageDon = "pomme-bordeaux.png";
-    break;
-    case "flower" :
-      $imageDon = "pomme-mauve.png";
-    break;
-    default :
-    $imageDon = "pomme-jaune.png";
-    break;
+  if ($value["ads_type"] === "don") {
+    switch ($value["ads_category"]) {
+      case "seed":
+        $imageDon = "pomme-bleue.png";
+        break;
+      case "ground":
+        $imageDon = "pomme-bordeaux.png";
+        break;
+      case "flower":
+        $imageDon = "pomme-mauve.png";
+        break;
+      default:
+        $imageDon = "pomme-jaune.png";
+        break;
     }
- } else {
-  $imageDon = "pomme-rouge.png";
- };
-// création de l'annonce
+  } else {
+    $imageDon = "pomme-rouge.png";
+  };
+  // création de l'annonce
   $dons .= "<section class='box'>
               <div class='imageDon' style='background-image: url(public/image/" . $value["ads_picture"] . ")'>
               </div>
               <div class= 'dons'>
                 <div>
-                  <img class='pomme' src='./public/image/" .$imageDon. "' />
+                  <img class='pomme' src='./public/image/" . $imageDon . "' />
                   <h3>&nbsp;" . $value["ads_title"] . "</h3>
                 </div>
                 <div class='d-flex row'>
-                  <p>Date: ". $value["ads_date"] . "&nbsp;</p>
+                  <p>Date: " . $value["ads_date"] . "&nbsp;</p>
                   <p> &nbsp; </p>
                   <p>&nbsp;Lieu : Ottignies</p>
                 </div>
                 <article>" . $value["ads_description"] . "</article>
-<<<<<<< HEAD
-                <div><button class=". $value["ads_category"] ." ><a href='#'>Contact</a>
-                </button></div>
-=======
                 <div class='d-flex row'>
-                  <button class=". $value["ads_category"] ." ><a href='#'>Contact</a>
+                  <button class=" . $value["ads_category"] . " ><a href='#'>Contact</a>
                   </button>
                   <i class='far fa-heart'></i>
                 </div>
->>>>>>> ab5ac0f85bd20358fb30047e2f9300b885b56d5b
               </div>
             </section>";
 }
 
 
-    include("view/page/dons.php");
+include("view/page/dons.php");

be5cb85b41c0b0e94790f4803d2d6176493353ce
Daphné Delvaux <ddelvaux71@gmail.com>
2020-12-02 19:33:24 +0100

    poste annnonce formé ss reception ds données

--- public/js/script_dons.js
+++ public/js/script_dons.js
@@ -1,3 +1,5 @@
+// Tri
+
 const options = document.querySelector(".options");
 const chevron = document.querySelector(".fa-chevron-down");
 chevron.addEventListener("click", function () {
@@ -18,5 +20,76 @@ chevron.addEventListener("click", function () {
         options.style.display = "none";
     });
 }
+});
 
+// Montrer la Section CREATION d'ANNONCE 
+const enteteDons = document.querySelector(".enteteDons");
+const annonceDons = document.querySelector(".annonceDons");
+const posterAnnonce = document.querySelector(".posterAnnonce");
+posterAnnonce.style.display = "none";
+const btnDons = document.querySelector(".btnDons");
+btnDons.addEventListener("click", function (e) {
+    posterAnnonce.style.display = "block";
+    annonceDons.style.display = "none";
+    enteteDons.style.visibility = "hidden";
+});
+
+// Modifications selon CHOIX des CATEGORIES
+
+const categories = document.getElementsByName('categorie');
+const time = document.querySelector(".time");
+time.style.display = "none";
+const timeSlot = document.querySelector(".timeSlot");
+timeSlot.style.display = "none";
+const img = document.querySelector(".imageAnnonce");
+
+const cacher = (arguments) => {
+    img.style.border = "none";
+    time.style.display = "none";
+    timeSlot.style.display = "none";
+};
+
+for (let categorie of categories){
+    categorie.addEventListener("change", function (e) {
+        
+        switch(e.currentTarget.value) {
+            case "time":
+              img.style.backgroundImage = "url('../jolly_seed/public/image/avatar.png')";
+              img.style.border = "2px solid #6e9611";
+              time.style.display = "block";
+              var times = document.getElementsByName('time');
+              for (let tme of times){
+                tme.addEventListener("change", function (e) {
+                   if (e.currentTarget.value ==="offre") {
+                    timeSlot.style.display = "block";   
+                   };
+                });
+            }
+              break;
+            case "seed":
+              img.style.backgroundImage = "url('../jolly_seed/public/image/seed.jpg')";
+              cacher();
+              break;
+            case "flower":
+              img.style.backgroundImage = "url('../jolly_seed/public/image/tomates.jpg')";
+              cacher();
+              break;
+            case "ground":
+              img.style.backgroundImage = "url('../jolly_seed/public/image/copeaux.jpg')";
+              cacher();
+            break;
+              default:
+              img.style.backgroundImage = "url('../jolly_seed/public/image/chene.jpg')";
+              cacher();
+          };
+    });
+}
+
+// POSTER l'ANNONCE
+
+const creer = document.querySelector(".creer");
+creer.addEventListener("click", function (e) {
+    posterAnnonce.style.display = "none";
+    annonceDons.style.display = "flex";
+    enteteDons.style.visibility = "visible";
 });
--- view/page/dons.php
+++ view/page/dons.php
@@ -1,77 +1,88 @@
 <section class="pageDons">
-        <section class="centrePageDons">
-          <div class="d-flex row espace">
-            <a href="#">Voir sur la carte</a>
-            <button class="btnDons"><a href="#">Poster une annonce</a></button>
-            <div class="tri">
-              <p class="select">Trier par: <span class="choixTri">Date </span></p>
-              <i class="fas fa-chevron-down"></i>
-              <div class="options">
-                <a id="date" class="opt" href="#">Date</a>
-                <a id="categorie" class="opt" href="#">Catégorie</a>
-                <a id="proximite" class="opt" href="#">Proximité</a>
-              </div>
-            </div>
-          </div>
-          <div class="annonceDons">
-          <?=$dons?>
-          <div>
-        </section>
-      </section>
-      <section class="posterAnnonce box"> 
-        <h2>Création d'annonce</h2>
-        <form action="/action_page.php" method="post">
-         
-          <h3>Catégorie:</h3>
-          <input type="radio" id="seed" name="categorie" value="seed" />
-          <label for="seed">Graines / Semis</label><br/>
-          <input type="radio" id="flower" name="categorie" value="flower" />
-          <label for="flower">Fleurs / Fruits / Légumes </label><br/>
-          <input type="radio" id="ground" name="categorie" value="ground" />
-          <label for="ground">Terreau / Copeaux / Buches / Bois</label><br/>
-          <input type="radio" id="plant" name="categorie" value="plant" />
-          <label for="plant">Plants / Arbustres / Arbres</label><br/>
-          <input type="radio" id="time" name="categorie" value="time" />
-          <label for="time">Temps donné / Temps offert</label><br/>
-     
-          <h3>Image: </h3>
-          <div class="d-flex row">
-            <div class='imageAnnonce'>
-            </div>
-            <div>
-              <p>Afin d'illustrer vortre annonce, vous pouvez soit utiliser l'image ci-contre ou en télécharger une nouvelle.</p>
-              <input type="file" class="message-texte" name="file"><br />
-            </div>
-          </div>
-          <label for="titre"><h3>Titre:</h3></label>
-          <input type="text" id="titre" name="titre" /><br />
+  <section class="centrePageDons">
+    <div class="d-flex row enteteDons">
+      <a href="#">Voir sur la carte</a>
+      <button class="btnDons"><a href="#">Poster une annonce</a></button>
+      <div class="tri">
+        <p class="select">Trier par: <span class="choixTri">Date </span></p>
+        <i class="fas fa-chevron-down"></i>
+        <div class="options">
+          <a id="date" class="opt" href="#">Date</a>
+          <a id="categorie" class="opt" href="#">Catégorie</a>
+          <a id="proximite" class="opt" href="#">Proximité</a>
+        </div>
+      </div>
+    </div>
+    <div class="annonceDons">
+    <?=$dons?>
+    <div>
+  </section>
+</section>
+<section class="posterAnnonce box"> 
+  <h2>Création d'annonce</h2>
+  <form action="/action_page.php" method="post">
+    <h3>Catégorie:</h3>
+    <div class= "sectionCategorie">
+      <div class="radio">
+        <input type="radio" id="time" name="ads_category" value="time" />
+        <label for="time">Temps demandé / Temps offert</label><br/>
+        <input type="radio" id="seed" name="ads_category" value="seed" />
+        <label for="seed">Graines / Semences / Noyaux</label><br/>
+        <input type="radio" id="flower" name="ads_category" value="flower" checked/>
+        <label for="flower">Fleurs / Fruits / Légumes </label><br/>
+        <input type="radio" id="ground" name="ads_category" value="ground" />
+        <label for="ground">Terreau / Copeaux / Buches / Bois</label><br/>
+        <input type="radio" id="plant" name="ads_category" value="plant" />
+        <label for="plant">Plants / Arbustres / Arbres</label><br/>
+      </div>
+      <div class="time">
+        <input type="radio" id="demande" name="ads_title" value="demande" />
+        <label for="demande">Demande</label><br/>
+        <input type="radio" id="offre" name="ads_title" value="offre" />
+        <label for="offre">Offre</label><br/>
+        <div class="timeSlot">
+          <input type="radio" id="demiJournee" name="ads_time" value="demiJournee" />
+          <label for="demiJournee">Une demi-journée</label><br/>
+          <input type="radio" id="journee" name="ads_time" value="journee" />
+          <label for="journee">Une journee</label><br/>
+          <input type="radio" id="plus" name="ads_time" value="plus" />
+          <label for="plus">Plus d'une journée</label><br/>
+        </div>
+      </div>
+    </div>
+    <h3>Image: </h3>
+    <div class="sectionCategorie annonceImage">
+      <div class='imageAnnonce'>
+      </div>
+      <div class="imageAlternative">
+        <p>Afin d'illustrer vortre annonce, vous pouvez soit utiliser l'image ci-contre ou en télécharger une nouvelle.</p>
+        <input type="file" class="message-texte" name="ads_picture"><br />
+      </div>
+    </div>
+    <label for="titre"><h3>Titre:</h3></label>
+    <input type="text" id="titre" name="ads_title" placeholder="nombre de char à fixer"/><br />
 
-          <label for="description"><h3>Description:</h3></label>
-          <textarea id="description" name="description" rows="4" cols="50"> </textarea><br />
+    <label for="description"><h3>Description:</h3></label>
+    <textarea id="description" name="ads_description" rows="4" cols="40"> </textarea><br />
 
-          <label for="contact"><h3>Contact:</h3></label>
-          <p> Pour la prise de contact, je désire partager :</p>
-          <div class="d-flex column">
-            <input type="checkbox" id="contactNom" name="contactNom" value="nom">
-            <label for="contactNom"> Nom de famille</label><br>
-            <input type="checkbox" id="contactPrenom" name="contactPrenom" value="prenom">
-            <label for="contactPrenom"> Prénom</label><br>
-            <input type="checkbox" id="adresse mail" name="adresse mail" value="mail">
-            <label for="adresse mail"> Adresse mail</label><br>        
-            <input type="checkbox" id="noTel" name="noTel" value="tel">
-            <label for="noTel"> Numéro de téléphone</label><br>
-            <input type="checkbox" id="intro" name="intro" value="intro">
-            <label for="intro"> Paragraphe d'introdution</label><br>
-          </div>
-          <p>Si toutes les cases sont décochées, les seul moyen de contact sera via le site.</p>
-        
-
-
-          <button><a href="#">Créer l'annonce</a></button>
-        </form>
-
-
-
-
-      </section>
-      <script src="public/js/script_dons.js"></script>
+    <label for="contact"><h3>Contact:</h3></label>
+    <p> Pour la prise de contact, je désire partager :</p>
+    <div class="left">
+      <input type="checkbox" id="contactNom" name="contactNom" value="nom">
+      <label for="contactNom"> Nom de famille</label><br/>
+      <input type="checkbox" id="contactPrenom" name="contactPrenom" value="prenom">
+      <label for="contactPrenom"> Prénom</label><br/>
+      <input type="checkbox" id="photo" name="photo" value="mail">
+      <label for="photo"> Photo personnelle</label><br/>  
+      <input type="checkbox" id="adresse mail" name="adresse mail" value="mail">
+      <label for="adresse mail"> Adresse mail</label><br/>        
+      <input type="checkbox" id="noTel" name="noTel" value="tel">
+      <label for="noTel"> Numéro de téléphone</label><br/>
+      <input type="checkbox" id="intro" name="intro" value="intro">
+      <label for="intro"> Paragraphe d'introdution</label><br/>
+    </div>
+    <p>Si toutes les cases sont décochées, le seul moyen de contact sera via le site.</p>
+    <button class="creer"><a href="#">Créer l'annonce</a></button>
+  </form>
+</section>
+<script src="public/js/script_dons.js"></script>

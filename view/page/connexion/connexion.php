27cdb3409b130c3a4b324d302e8fcb1220ecdda4
LaetitiaDebruyne <67645912+LaetitiaDebruyne@users.noreply.github.com>
2020-12-02 09:05:16 +0100

    add inscription

--- view/page/connexion/connexion.php
+++ view/page/connexion/connexion.php
@@ -1,3 +1,38 @@
+<!--
+<section class="container-connexion">
+    <h2>Connexion</h2>
+    <fieldset>
+        <form action="/formulaire" method="post">
+            <div class="nom">
+                <label for="nom">Nom d'utilisateur</label><br />
+                <input type="text" placeholder="Entrer le nom d'utilisateur" name="nom" class="input" id="nom" required />
+            </div>
+            <br/>
+
+            <div class="motdepasse">
+                <label for="motdepasse">Mot de passe</label><br />
+                <input type="text" placeholder="Entrer le mot de passe" name="motdepasse" class="input" id="motdepasse" required />
+            </div>
+            <br />
+
+            <div>
+                <input type="checkbox" id="checkbox" class="input" name="checkbox" checked />
+                <label for="checkbox">Se souvenir de moi</label>
+            </div>
+            <br />
+
+            <div>
+                <button disabled type="submit" id="bouton">Se connecter</button>
+            </div>
+            <br />
+            <div><button type="reset">Effacer</button></div>
+        </form>
+    </fieldset>
+    <a href id="forget-password">Mot de passe oublié ?</a>
+</section>
+-->
+
+
 <h2>Connexion</h2>
 <form action="#" method="post">
     <label for="nom">Nom :</label>
@@ -6,3 +41,5 @@
     <input type="text" name="prenom"><br>
     <input type="submit" value="Connexion">
 </form>
+
+
---
+++ view/page/inscription.php
@@ -0,0 +1,62 @@
+<!--
+<section class="container-inscription">
+    <h2>Inscription</h2>
+    <fieldset>
+        <form action="/formulaire" method="post">
+            <div class="prenom">
+                <label for="prenom"></label>
+                <input type="text" placeholder="Prénom" name="prenom" class="input" id="prenom" required>
+            </div><br />
+
+            <div class="nom">
+                <label for="nom"></label>
+                <input type="text" placeholder="Nom de famille" name="nom" class="input" id="nom" required>
+            </div><br />
+
+            <div class="email">
+                <label for="email"></label>
+                <input type="text" placeholder="E-mail" name="email" class="input" id="email" required>
+            </div><br />
+
+            <div class="rue">
+                <label for="rue"></label>
+                <input type="text" placeholder="Rue" name="rue" class="input" id="rue" required>
+            </div><br />
+
+            <div class="numero">
+                <label for="numero"></label>
+                <input type="text" placeholder="Numéro" name="numero" class="input" id="numero" required>
+            </div>
+
+            <div class="codepostal">
+                <label for="codepostal"></label>
+                <input type="text" placeholder="Code Postal" name="codepostal" class="input" id="codepostal" required>
+            </div><br />
+
+            <br />
+            <div class="ville">
+                <label for="ville"></label>
+                <input type="text" placeholder="Ville" name="ville" class="input" id="ville" required>
+            </div><br />
+
+            <div class="motdepasse">
+                <label for="motdepasse"></label>
+                <input type="text" placeholder="Mot de passe" name="email" class="input" id="email" required>
+            </div><br />
+
+            <div class="telephone">
+                <label for="telephone"></label>
+                <input type="text" placeholder="Téléphone fixe ou mobile" name="telephone" class="input" id="telephone" required>
+            </div><br />
+
+            <div>
+                <input type="checkbox" id="checkbox" name="checkbox" checked>
+                <label for="checkbox"><a>J'accepte les conditions générales</a></label>
+            </div><br />
+
+            <div><button disabled type="submit" id="bouton">S'inscrire</button></div><br>
+            <div><button type="reset">Effacer</button></div>
+        </form>
+    </fieldset>
+</section>
+-->

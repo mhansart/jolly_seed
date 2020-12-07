29d96ee59000ca3bd62388d350740d681d0e434e
LaetitiaDebruyne <67645912+LaetitiaDebruyne@users.noreply.github.com>
2020-12-07 12:18:54 +0100

    ajout des témoignages

--- view/page/temoignages.php
+++ view/page/temoignages.php
@@ -1,1 +1,15 @@
 <h2>Témoignages</h2>
+
+
+
+<p>Ce site rend accessible le jardinage à tous, peu importe le revenu. Cet année, je vise un jardin urbain complètement gratuit et c'est possible grâce à des sites de partage comme JollySeed. Ce concept brise l'isolement, crée des liens, sensibilise aux avantages indéniables du partage de connaissances et de matériaux. Il devrait y avoir des initiatives de ce genre dans tous les domaines: partage d'outils par voisinages, de films, de livres, etc.
+    Merci et bravo, bon jardinage à tous ! </p>
+
+<p>J'adore JollySeed !! C'est un outil merveilleux qui me permet de donner des plantes qu'ai en trop. Ça donne aussi l'occasion de découvrir de nouvelles plantes. J'aime beaucoup ce principe de partage et de dons. Je fais de belles rencontres avec d'autres jardiniers. C'est une excellente façon de partager notre passion du jardinage et d'échanger des connaissances horticoles. On tisse des liens avec d'autres jardiniers.
+    Longue vie à JollySeed ! </p>
+
+<p>Je suis très content d'avoir trouvé preneur pour mes papyrus! Ça aurait été dommage de devoir les jeter - en fait, je crois que je n'aurais jamais réussi à le faire, donc tout est bien qui fini bien! Super site. C'est juste dommage qu'il n'y aie pas autant de participant que j'aurai voulu dans mon secteur. Merci ! </p>
+
+<p>Je m'appelle David, je suis en reconversion professionnelle pour devenir jardinier agro-écologique, j'ai réussi à créer un magnifique potager chez Claudine, qui n'a plus l'énergie de cultiver son jardin. Une très belle rencontre. Tout ça grâce à JollySeed !</p>
+
+<p>Je n'ai jamais eu un jardin aussi beau et bien cultivé grâce à David, un homme passionné de jardinage et plein d'idées !</p>

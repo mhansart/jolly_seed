0a977ece3babc5079ce23e1b87820a61b93a0d02
Daphné Delvaux <ddelvaux71@gmail.com>
2020-12-23 10:48:00 +0100

    pr très ptts tél

--- view/page/a_propos.php
+++ view/page/a_propos.php
@@ -1,7 +1,7 @@
 <section class="a_propos">
     <h2>A propos</h2>
     <p>Jolly-Seed a été créé à l'occasion d'un travail de groupe dans le cadre d'études de Front-End developer à Interface3. Différentes idées avaient été proposées par l'un et l'autre et toutes les trois ont vibré pour ce projet de partage.</p>
-    <p>Un 'Brainstorming' s'en est suivi pour trouver le nom, le logo la charte graphique etc. N'ayant eu que peu de cours de PHP, il était malgré tout évident qu'un passage par là serait indispensable à la gestion de la base de données qu'on s'était imaginée. Ayant eu de nombreux cours de Javascript sans pour autant avoir vu les requetes Ajax, nous nous sommes plongées plus amplement dans le PHP.</p> 
+    <p>Un 'Brainstorming' s'en est suivi pour trouver le nom, le logo la charte graphique etc. N'ayant eu que peu de cours de PHP, il était malgré tout évident qu'un passage par là serait indispensable à la gestion de la base de données qui avait imaginée. Ayant eu de nombreux cours de Javascript sans pour autant avoir vu les requetes Ajax, nous nous sommes plongées plus amplement dans le PHP.</p> 
     <p>Lors de la réalisation de la carte, il est apparu qu'une approche d'Ajax était malgré tout indispensable... </p>
     <p>Un petit mot sur les trois conceptrices:</p>
     <div class="conceptrice">

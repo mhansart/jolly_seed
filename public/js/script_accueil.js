9c7c0a0b5e712eaf667ac90d15089bfd8de4c682
Hansart Marine <hansart.marine@gmail.com4>
2021-01-04 14:41:27 +0100

    (master): add button reactivate ads with ajax request

--- public/js/script_accueil.js
+++ public/js/script_accueil.js
@@ -2,6 +2,31 @@ const accueilMenu = document.getElementById("accueil-menu");
 if (accueilMenu) {
   accueilMenu.classList.add("active");
 }
+const headerMenu = document.querySelector('header');
+const container = document.querySelector(".contact-container");
+if (container) {
+  const getSizeHeaderMenu = () => {
+    const widthWindow = window.innerWidth;
+    const heightHeaderMenu = parseInt(getComputedStyle(headerMenu).height, 10);
+    if (heightHeaderMenu > 140) {
+      container.style.marginTop = `-0px`;
+    }
+    if (heightHeaderMenu < 90 && widthWindow <= 600) {
+      container.style.marginTop = `-60px`;
+    }
+    if (heightHeaderMenu < 90 && widthWindow <= 1035 && widthWindow > 600) {
+      container.style.marginTop = `-110px`;
+    }
+    if (heightHeaderMenu < 90 && widthWindow > 1035) {
+      container.style.marginTop = `-60px`;
+    }
+  };
+  getSizeHeaderMenu();
+  window.addEventListener('resize', function(){
+    
+    getSizeHeaderMenu();
+});
+}
 
 //CREATION de CARTE
 const carte = L.map("mapid");
@@ -69,77 +94,86 @@ function ajaxGet(url) {
 }
 
 //requete pour chercherche les données ads
-ajaxGet("http://localhost/jolly_seed/ajax/ads.php").then(reponse => {
-    let ads = JSON.parse(reponse)
-    console.log("ajax:", ads)
-    for (let ad of ads){
-        if (ad[1]==="jardinier"){
-            ajaxGet(`https://nominatim.openstreetmap.org/search?q=${ad[5]}&format=json&addressdetails1&limit=1`).then(reponse => {
-                //conversion en Javascript:
-                let data = JSON.parse(reponse)
-                console.log(data)
-                let coord = [data[0].lat, data[0].lon]
-                console.log(coord)
-                const marker = L.marker(coord,{icon: iconeTime})
-                marker.addTo(carte)
-                marker.bindPopup(`<h5>${ad[4]}<h5>`);
-                })
-        } else {
-            switch(ad[2]) {
-                case "flower":
-                    //requete pour transformer adresses en coordonnées
-                    ajaxGet(`https://nominatim.openstreetmap.org/search?q=${ad[5]}&format=json&addressdetails1&limit=1`).then(reponse => {
-                    //conversion en Javascript:
-                    let data = JSON.parse(reponse)
-                    console.log(data)
-                    let coord = [data[0].lat, data[0].lon]
-                    console.log(coord)
-                    const marker = L.marker(coord,{icon: iconeFlower})
-                    marker.addTo(carte)
-                    marker.bindPopup(`<h5>${ad[4]}<h5>`);
-                    })
-                  break;
-                case "seed":
-                    ajaxGet(`https://nominatim.openstreetmap.org/search?q=${ad[5]}&format=json&addressdetails1&limit=1`).then(reponse => {
-                        //console.log("reponse ", reponse)
-                        //conversion en Javascript:
-                        let data = JSON.parse(reponse)
-                        let coord = [data[0].lat, data[0].lon]
-                        console.log(coord)
-                        const marker = L.marker(coord,{icon: iconeSeed})
-                        marker.addTo(carte)
-                        if(ad[4]==="Offert"){
-                            marker.bindPopup(`<h5>Temps offert<h5>`);
-                        } else {
-                            marker.bindPopup(`<h5>Temps demandé<h5>`);   
-                        }
-                        })
-                    break;
-                case "ground":
-                    ajaxGet(`https://nominatim.openstreetmap.org/search?q=${ad[5]}&format=json&addressdetails1&limit=1`).then(reponse => {
-                        //console.log("reponse ", reponse)
-                        //conversion en Javascript:
-                        let data = JSON.parse(reponse)
-                        let coord = [data[0].lat, data[0].lon]
-                        console.log(coord)
-                        const marker = L.marker(coord,{icon: iconeGround})
-                        marker.addTo(carte)
-                        marker.bindPopup(`<h5>${ad[4]}<h5>`);
-                        })
-                    break;
-                default:
-                    ajaxGet(`https://nominatim.openstreetmap.org/search?q=${ad[5]}&format=json&addressdetails1&limit=1`).then(reponse => {
-                        //console.log("reponse ", reponse)
-                        //conversion en Javascript:
-                        let data = JSON.parse(reponse)
-                        let coord = [data[0].lat, data[0].lon]
-                        console.log(coord)
-                        const marker = L.marker(coord,{icon: iconePlant})
-                        marker.addTo(carte)
-                        marker.bindPopup(`<h5>${ad[4]}<h5>`);
-                        })  
-            };
-        }
+ajaxGet("http://localhost/jolly_seed/ajax/ads.php").then((reponse) => {
+  let ads = JSON.parse(reponse);
+  console.log("ajax:", ads);
+  for (let ad of ads) {
+    if (ad[1] === "jardinier") {
+      ajaxGet(
+        `https://nominatim.openstreetmap.org/search?q=${ad[5]}&format=json&addressdetails1&limit=1`
+      ).then((reponse) => {
+        //conversion en Javascript:
+        let data = JSON.parse(reponse);
+        console.log(data);
+        let coord = [data[0].lat, data[0].lon];
+        console.log(coord);
+        const marker = L.marker(coord, { icon: iconeTime });
+        marker.addTo(carte);
+        marker.bindPopup(`<h5>${ad[4]}<h5>`);
+      });
+    } else {
+      switch (ad[2]) {
+        case "flower":
+          //requete pour transformer adresses en coordonnées
+          ajaxGet(
+            `https://nominatim.openstreetmap.org/search?q=${ad[5]}&format=json&addressdetails1&limit=1`
+          ).then((reponse) => {
+            //conversion en Javascript:
+            let data = JSON.parse(reponse);
+            console.log(data);
+            let coord = [data[0].lat, data[0].lon];
+            console.log(coord);
+            const marker = L.marker(coord, { icon: iconeFlower });
+            marker.addTo(carte);
+            marker.bindPopup(`<h5>${ad[4]}<h5>`);
+          });
+          break;
+        case "seed":
+          ajaxGet(
+            `https://nominatim.openstreetmap.org/search?q=${ad[5]}&format=json&addressdetails1&limit=1`
+          ).then((reponse) => {
+            //console.log("reponse ", reponse)
+            //conversion en Javascript:
+            let data = JSON.parse(reponse);
+            let coord = [data[0].lat, data[0].lon];
+            console.log(coord);
+            const marker = L.marker(coord, { icon: iconeSeed });
+            marker.addTo(carte);
+            if (ad[4] === "Offert") {
+              marker.bindPopup(`<h5>Temps offert<h5>`);
+            } else {
+              marker.bindPopup(`<h5>Temps demandé<h5>`);
+            }
+          });
+          break;
+        case "ground":
+          ajaxGet(
+            `https://nominatim.openstreetmap.org/search?q=${ad[5]}&format=json&addressdetails1&limit=1`
+          ).then((reponse) => {
+            //console.log("reponse ", reponse)
+            //conversion en Javascript:
+            let data = JSON.parse(reponse);
+            let coord = [data[0].lat, data[0].lon];
+            console.log(coord);
+            const marker = L.marker(coord, { icon: iconeGround });
+            marker.addTo(carte);
+            marker.bindPopup(`<h5>${ad[4]}<h5>`);
+          });
+          break;
+        default:
+          ajaxGet(
+            `https://nominatim.openstreetmap.org/search?q=${ad[5]}&format=json&addressdetails1&limit=1`
+          ).then((reponse) => {
+            //console.log("reponse ", reponse)
+            //conversion en Javascript:
+            let data = JSON.parse(reponse);
+            let coord = [data[0].lat, data[0].lon];
+            console.log(coord);
+            const marker = L.marker(coord, { icon: iconePlant });
+            marker.addTo(carte);
+            marker.bindPopup(`<h5>${ad[4]}<h5>`);
+          });
+      }
     }
-  
+  }
 });

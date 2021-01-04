44d1d21385deacef2f68326f1c59f9e27e0610b4
Hansart Marine <hansart.marine@gmail.com4>
2021-01-04 08:23:22 +0100

    (master): add script to show picture automatically

--- public/js/script.js
+++ public/js/script.js
@@ -1,94 +1,92 @@
 var h = window.innerHeight;
-var style = document.createElement('style');
+var style = document.createElement("style");
 document.head.appendChild(style);
 style.sheet.insertRule(`body {height: ${h}px}`);
 
-const subHeader = document.querySelector('.subheader');
+const subHeader = document.querySelector(".subheader");
 
-const header = document.querySelector('header');
-const heightHeader = parseInt(getComputedStyle(header).height,10);
-const main = document.querySelector('main');
-const footer = document.querySelector('footer');
-const heightFooter = parseInt(getComputedStyle(footer).height,10);
-const containerChat = document.querySelector('.chat-container');
+const header = document.querySelector("header");
+const heightHeader = parseInt(getComputedStyle(header).height, 10);
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+const heightFooter = parseInt(getComputedStyle(footer).height, 10);
+const containerChat = document.querySelector(".chat-container");
 
-const getSizeMain = ()=>{
-    if(containerChat){
-        main.style.minHeight = `${h}px`;
-    }else{
-        main.style.minHeight = `${h-heightFooter-19}px`;
-    }
-}
+const getSizeMain = () => {
+  if (containerChat) {
+    main.style.minHeight = `${h}px`;
+  } else {
+    main.style.minHeight = `${h - heightFooter - 19}px`;
+  }
+};
 getSizeMain();
 
-window.addEventListener('resize', function(){
-    getSizeMain();
+window.addEventListener("resize", function () {
+  getSizeMain();
 });
 
-const heightMain = parseInt(getComputedStyle(main).minHeight,10);
-console.log(heightMain);
+const heightMain = parseInt(getComputedStyle(main).minHeight, 10);
 
-
-if(containerChat){
-    footer.style.display="none";
-    const getSizeChat = ()=>{
-        const paddingMain = parseInt(getComputedStyle(main).paddingTop,10);
-        containerChat.style.height = `${heightMain - paddingMain}px`;
-    }
+if (containerChat) {
+  footer.style.display = "none";
+  const getSizeChat = () => {
+    const paddingMain = parseInt(getComputedStyle(main).paddingTop, 10);
+    containerChat.style.height = `${heightMain - paddingMain}px`;
+  };
+  getSizeChat();
+  window.addEventListener("resize", function () {
     getSizeChat();
-    window.addEventListener('resize', function(){
-        getSizeChat();
-    });
+  });
 }
 
-if(subHeader !== null){
-    window.addEventListener('scroll', () => {
-        (function scroll() {
-            const headerMenu = document.querySelector('header');
-            if(headerMenu) {
-                if(window.pageYOffset > 100){
-                    headerMenu.classList.add('reduce');
-                } else {
-                    headerMenu.classList.remove('reduce');
-                }
-            }
-        })();
-    });
-}
-
-const menuBurger = document.getElementById('menu-burger');
-const menuNav = document.querySelector('.nav-connected');
-const navBar = document.querySelector('.nav-pages');
-
-if(menuNav){
-    const getSizeNav = ()=>{
-        var w = window.innerWidth;
-        if(parseInt(w)<600){
-            menuNav.style.height = `${h}px`;
-        }else{
-            menuNav.style.height = '100%';
+if (subHeader !== null) {
+  window.addEventListener("scroll", () => {
+    (function scroll() {
+      const headerMenu = document.querySelector("header");
+      if (headerMenu) {
+        if (window.pageYOffset > 100) {
+          headerMenu.classList.add("reduce");
+        } else {
+          headerMenu.classList.remove("reduce");
         }
+      }
+    })();
+  });
+}
+
+const menuBurger = document.getElementById("menu-burger");
+const menuNav = document.querySelector(".nav-connected");
+const navBar = document.querySelector(".nav-pages");
+
+if (menuNav) {
+  const getSizeNav = () => {
+    var w = window.innerWidth;
+    if (parseInt(w) < 600) {
+      menuNav.style.height = `${h}px`;
+    } else {
+      menuNav.style.height = "100%";
     }
-    getSizeNav();
+  };
+  getSizeNav();
 
-    menuBurger.addEventListener('click', function(){
-        if(!menuNav.classList.contains('open-menu')){
-            menuNav.classList.add('open-menu');  
-        }else{
-            menuNav.classList.remove('open-menu');
-        }
-    })
-    window.addEventListener('resize', function(){
-            getSizeNav();
-    });
+  menuBurger.addEventListener("click", function () {
+    if (!menuNav.classList.contains("open-menu")) {
+      menuNav.classList.add("open-menu");
+    } else {
+      menuNav.classList.remove("open-menu");
+    }
+  });
+  window.addEventListener("resize", function () {
+    getSizeNav();
+  });
 }
 
 const searchBarGeneral = document.getElementById("search-bar-general");
-if(searchBarGeneral){
-        searchBarGeneral.addEventListener("keyup", function(event) {
-        event.preventDefault();
-        if (event.key === "Enter") {
-            document.getElementById("search-bar-general-button").click();
-        }
-    });
+if (searchBarGeneral) {
+  searchBarGeneral.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.key === "Enter") {
+      document.getElementById("search-bar-general-button").click();
+    }
+  });
 }
--- public/js/script_moncompte.js
+++ public/js/script_moncompte.js
@@ -1,110 +1,127 @@
 var h = window.innerHeight;
-var style = document.createElement('style');
+var style = document.createElement("style");
 document.head.appendChild(style);
 style.sheet.insertRule(`body {height: ${h}px}`);
 
-const btnModif = document.getElementById('modif-infos-mon-compte');
-const formModif = document.querySelector('.modif-infos-moncompte');
-const btnModifPicture = document.querySelector('.btn-modif-picture-moncompte');
-const btnDelete = document.querySelector('.delete-mon-compte');
-const formDelete = document.querySelector('.delete-infos-moncompte');
-const formModifPicture=document.querySelector('.form-modif-picture-moncompte');
-const overlay = document.getElementById('overlay');
+const btnModif = document.getElementById("modif-infos-mon-compte");
+const formModif = document.querySelector(".modif-infos-moncompte");
+const btnModifPicture = document.querySelector(".btn-modif-picture-moncompte");
+const btnDelete = document.querySelector(".delete-mon-compte");
+const formDelete = document.querySelector(".delete-infos-moncompte");
+const formModifPicture = document.querySelector(
+  ".form-modif-picture-moncompte"
+);
+const overlay = document.getElementById("overlay");
 const menuMoncompte = document.querySelector(".menu-mon-compte");
 const menuActive = document.getElementById("subheader-mon-compte");
 const menuMonCompte = document.getElementById("moncompte-menu");
 const menuMessagerie = document.getElementById("menu-messagerie-a");
 const messagerieHidden = document.querySelector(".allMsg-menu");
 
-const headerGet = document.querySelector('header');
+const headerGet = document.querySelector("header");
 const heightHeaderGet = getComputedStyle(headerGet).height;
 
+menuActive.classList.add("active");
+menuMonCompte.classList.add("active");
 
-menuActive.classList.add('active');
-menuMonCompte.classList.add('active');
-
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   (function scroll() {
-      if(menuMoncompte) {
-          if(window.pageYOffset > 100){
-              menuMoncompte.classList.add('reduce-mon-compte');
-          } else {
-              menuMoncompte.classList.remove('reduce-mon-compte');
-          }
+    if (menuMoncompte) {
+      if (window.pageYOffset > 100) {
+        menuMoncompte.classList.add("reduce-mon-compte");
+      } else {
+        menuMoncompte.classList.remove("reduce-mon-compte");
       }
+    }
   })();
 });
 
-menuMessagerie.addEventListener('click', ()=>{
-    messagerieHidden.style.height = getComputedStyle(messagerieHidden).height === "0px"? `${h -parseInt(heightHeaderGet,10)-70}px`:'0px';
+menuMessagerie.addEventListener("click", () => {
+  messagerieHidden.style.height =
+    getComputedStyle(messagerieHidden).height === "0px"
+      ? `${h - parseInt(heightHeaderGet, 10) - 70}px`
+      : "0px";
 });
 
-
-
-if(btnModif){
-    btnModif.addEventListener('click', ()=>{
-        formModif.classList.toggle('d-flex');
-        overlay.classList.toggle('d-flex');
-
-    })
-    btnModifPicture.addEventListener('click', ()=>{
-        formModifPicture.classList.toggle('d-flex');
-        overlay.classList.toggle('d-flex');
-    })
-    btnDelete.addEventListener('click', ()=>{
-      formDelete.classList.toggle('d-flex');
-      overlay.classList.toggle('d-flex');
-    })
-    const close = document.querySelectorAll('.close');
-    close.forEach((btn)=>{
-        btn.addEventListener('click', ()=>{
-            overlay.classList.toggle('d-flex');
-            if(formModifPicture.classList.contains('d-flex')){
-                formModifPicture.classList.toggle('d-flex');  
-            }else if(formModif.classList.contains('d-flex')){
-                formModif.classList.toggle('d-flex');
-            }else{
-              formDelete.classList.toggle('d-flex');
-            }
-            btnModifPicture.style.display="block";
-        })
-    })
-
+if (btnModif) {
+  btnModif.addEventListener("click", () => {
+    formModif.classList.toggle("d-flex");
+    overlay.classList.toggle("d-flex");
+  });
+  btnModifPicture.addEventListener("click", () => {
+    formModifPicture.classList.toggle("d-flex");
+    overlay.classList.toggle("d-flex");
+  });
+  btnDelete.addEventListener("click", () => {
+    formDelete.classList.toggle("d-flex");
+    overlay.classList.toggle("d-flex");
+  });
+  const close = document.querySelectorAll(".close");
+  close.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      overlay.classList.toggle("d-flex");
+      if (formModifPicture.classList.contains("d-flex")) {
+        formModifPicture.classList.toggle("d-flex");
+      } else if (formModif.classList.contains("d-flex")) {
+        formModif.classList.toggle("d-flex");
+      } else {
+        formDelete.classList.toggle("d-flex");
+      }
+      btnModifPicture.style.display = "block";
+    });
+  });
 }
 // recherche dans le menu messagerie
 
 const interlocuteur = document.querySelectorAll(".user-interlocuteur");
 const oneChat = document.querySelectorAll(".oneChat");
-const searchBar = document.querySelector('.ipt-search-msg');
+const searchBar = document.querySelector(".ipt-search-msg");
 
 const interlocuteurs = [];
-interlocuteur.forEach((x)=>{
-    interlocuteurs.push(x)
-})
+interlocuteur.forEach((x) => {
+  interlocuteurs.push(x);
+});
 
 const render = (arr) => {
-    oneChat.forEach((elt) => {
-      elt.style.display = "none";
-    });
-    arr.forEach((elt) => {
-      elt.style.display = "block";
-    });
-  };
-  const searchWord = (str) => {
-    const result = interlocuteurs.filter((elt) => {
-      return elt.value.toLowerCase().includes(str);
-    });
-    const resultArr = result.map((elt) => {
-      return elt.closest(".oneChat");
-    });
-  
-    uniqueArray = resultArr.filter(function (item, pos) {
-      return resultArr.indexOf(item) == pos;
-    });
-    render(uniqueArray);
-  };
-  
-  searchBar.addEventListener("input", (e) => {
-    const iptValue = e.currentTarget.value.toLowerCase();
-    searchWord(iptValue);
+  oneChat.forEach((elt) => {
+    elt.style.display = "none";
   });
+  arr.forEach((elt) => {
+    elt.style.display = "block";
+  });
+};
+const searchWord = (str) => {
+  const result = interlocuteurs.filter((elt) => {
+    return elt.value.toLowerCase().includes(str);
+  });
+  const resultArr = result.map((elt) => {
+    return elt.closest(".oneChat");
+  });
+
+  uniqueArray = resultArr.filter(function (item, pos) {
+    return resultArr.indexOf(item) == pos;
+  });
+  render(uniqueArray);
+};
+
+searchBar.addEventListener("input", (e) => {
+  const iptValue = e.currentTarget.value.toLowerCase();
+  searchWord(iptValue);
+});
+
+// afficher l'image après le choix dans les fichiers
+const loadFile = function (e) {
+  const reader = new FileReader();
+  reader.onload = function () {
+    const imgTry = document.getElementById("img-modif-essai");
+    imgTry.src = reader.result;
+    imgTry.style.display = "inline-flex";
+  };
+  // generer un url
+  reader.readAsDataURL(e.target.files[0]);
+};
+
+const iptImgPp = document.getElementById("user-picture");
+iptImgPp.addEventListener("change", (e) => {
+  loadFile(e);
+});

b2a1c41f2f7d04076d9fa9a81a46a870efdad637
Hansart Marine <hansart.marine@gmail.com4>
2021-01-11 11:54:52 +0100

    (master): debug chat

--- controller/deconnexionController.php
+++ controller/deconnexionController.php
@@ -5,4 +5,4 @@ unset($_SESSION["nom"], $_SESSION["prenom"], $_SESSION["user_id"]);
 // supprimer la session
 session_destroy();
 // rediriger
-header("Location:?section=connexion");
+header("Location:?section=accueil");
